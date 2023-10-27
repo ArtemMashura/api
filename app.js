561dec0cd2867c622aece7ece98657df71841e3d
ArtemMashura <128703506+ArtemMashura@users.noreply.github.com>
2023-10-28 00:04:38 +0300

    UI change, added funcionality to check users posts

--- app.js
+++ app.js
@@ -2,63 +2,119 @@ const API_USERS = 'https://jsonplaceholder.typicode.com/users';
 const API_ALBUMS = 'https://jsonplaceholder.typicode.com/albums';
 const API_PHOTOS = 'https://jsonplaceholder.typicode.com/photos';
 
-const API_SINGLE_USER = 'https://jsonplaceholder.typicode.com/todos?userId=';
+const API_SINGLE_USER = ' https://jsonplaceholder.typicode.com/posts?userId=';
 
+
+customElements.define(
+    "user-post",
+    class extends HTMLElement {
+      constructor() {
+        super();
+        const template = document.getElementById(
+          "post-template",
+        ).content;
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(template.cloneNode(true));
+      }
+    },
+);
+
+customElements.define(
+    "user-card",
+    class extends HTMLElement {
+        constructor() {
+            super();
+            const template = document.getElementById(
+            "user-template",
+            ).content;
+            const shadowRoot = this.attachShadow({ mode: "open" });
+            shadowRoot.appendChild(template.cloneNode(true));
+        }
+        
+        btnClick(callback) {
+            this.shadowRoot.querySelector('.btn-toPosts').addEventListener('click', callback);
+        }
+    },
+)
 
 class ApiReader {
     constructor(sourceId = 'source') {
         this.source = document.querySelector(`#${sourceId}`)
     }
-    
-    async getAllUsers(){
-        let data = await fetch(API_USERS)
-        return data.json()
+
+    static getData(url) {
+        return fetch(url)
+            .then((res) => { 
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}, Text: ${res.statusText}`);
+                }
+                return res.json();
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error.message);
+                throw error; 
+            });
+    }
+
+    static getAllUsers(){
+        const url = 'https://jsonplaceholder.typicode.com/users';
+        return this.getData(url)
+    }
+
+    static getUser(id){
+        const url = `https://jsonplaceholder.typicode.com/posts?userId=${id}`;
+        return this.getData(url);
+    }
+
+}
+
+class Renderer{
+    constructor(){
+        this.users = [];
+        this.posts = [];
     }
 
     async renderUsers(){
-        let template = '<table class="table">';
-        await this.getAllUsers().then(data => {
+        let result = document.getElementById(`result`);
+        result.innerHTML = ``;
+        await ApiReader.getAllUsers().then(data => {
             console.log(data)
             data.forEach(item =>{
-                template += `
-                    <tr>
-                        <td class="table-primary">${item.id}</td>
-                        <td class="table-secondary">${item.name}</td>
-                        <td class="table-light">${item.username}</td>
-                        <td class="table-success">${item.email}</td>
-                        <td class="table-info">${item.phone}</td>
-                    </tr>    
+                let template = document.createElement('user-card');
+                template.innerHTML = `
+                    <span slot="user-name">${item.name}</span>
+                    <span slot="user-userName">${item.username}</span>
+                    <span slot="user-phone">${item.phone}</span>
+                    <span slot="user-email">${item.email}</span>
                 `;
+                result.appendChild(template)
+
+                template.btnClick( () => {
+                    this.renderUserTasks(parseInt(item.id))
+                })
             });
         });
-        template += '</table>';
-        document.getElementById(`result`).innerHTML = template; // доводиться робити вибір елементу тут через те що наш джаваскрипт файл
-                                                                // потрібно ініціалізувати в початку файла для того щоб при ініціалізації
-                                                                // кнопка яка обирає усіх користувачів змогла законектити онклік івент
-                                                                // з цим файлом
-    }
-
-    async getUser(id){
-        let data = await fetch(API_SINGLE_USER.concat(id))
-        return data.json()
     }
 
     async renderUserTasks(id){
-        let template = '<table class="table">';
-        await this.getUser(id).then(data => {
+        let posts = document.getElementById(`posts`);
+        posts.innerHTML = ``;
+        await ApiReader.getUser(id).then(data => {
+            console.log(data)
+            
             data.forEach(item =>{
-                template += `
-                    <tr>
-                        <td class="table-primary">${item.id}</td>
-                        <td class="table-secondary">${item.title}</td>
-                        <td class="table-light">${item.completed}</td>
-                    </tr>    
+                let template = document.createElement('user-post');
+                template.innerHTML = `
+                    <span slot="postID">${item.id}</span>
+                    <span slot="title">${item.title}</span>
+                    <span slot="attributes">${item.body}</span>
                 `;
-            });
+                posts.appendChild(template)
+            })
         });
-        template += '</table>';
-        document.getElementById(`result`).innerHTML = template; // теж саме що й у минулій функції
-    }
+        return posts;
+    }     
 }
 
-const api = new ApiReader();
+
+const renderer = new Renderer();
